c51306d12d2c1f20a560f365c624ae2137273de9
phamvu1001 <117495509+phamvu1001@users.noreply.github.com>
2022-11-25 23:26:24 +0700

    Update WebServer.py

--- WebServer.py
+++ WebServer.py
@@ -1,36 +1,35 @@
 import socket
 import config
 from function.MethodParse import *
+from threading import Thread
+
+def Method(client):
+    request=getRequest(client)
+    if not request.empty:
+        print(f"-------------------\n [LISTENED REQUEST]\n Request catched: %s with %s has content %s\n"%(request.method, request.path, request.content))
+        if request.method == "POST":
+            postMethod(client, request)
+        else:
+            getMethod(client, request)
+	
+def Connections():
+    while True:
+        (client,address)=server.accept()
+        print(f"-------------------\n [SERVER]\n {address} sent request to server.")
+        Thread(target=Method,args=(client,)).start()
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+server.bind(('localhost', 8080))
 
 
 try:
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	server.bind((config.HOST, config.PORT))
-	server.listen(5)
-
-	print("-------------------\n [SERVER]\n Listening on: http://%s:%d"%(config.HOST,config.PORT))
-
-	while True:
-
-		#Server things
-		(client, address) = server.accept()
-
-		print(f"-------------------\n [SERVER]\n {address} sent request to server.")
-
-		#Get request
-		request = getRequest(client)
-
-		if not request.empty: 
-			print(f"-------------------\n [LISTENED REQUEST]\n Request catched: %s with %s has content %s\n"%(request.method, request.path, request.content))
-			if request.method == "POST":
-				postMethod(client, request)
-			else:
-				getMethod(client, request)
-
-		client.shutdown(socket.SHUT_RD)
-		print("\n--------[Request done]--------\n\n")
+    server.listen(5)
+        #client,addr=server.accept()
+    ACCEPT_THREAD=Thread(target=Connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
 except:
-    print('Error')
-
-client.close()
+    print("error\n")
+finally:
+    server.close()
  	
